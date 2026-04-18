88b5092e411028ba70914589620c769c3b1e93da
namanindoria <namanindoria2004@gmail.com>
2026-04-18 13:52:50 +0530

    just a mongoose import change

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
- const userSchema= new mongoose.Schema({
-  email:{
-    type:String,
-    required:true,
-    unique:true,
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  fullName:{
-    type:String,
-    required:true,
+  fullName: {
+    type: String,
+    required: true,
   },
-  password:{
-    type:String,
-    required:true,
-    minlength:6,
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
   },
-  profilePic:{
-    type:String,
-    default:""
+  profilePic: {
+    type: String,
+    default: ""
   }
 
 
- },
- {timestamps:true}
+},
+  { timestamps: true }
 );
-const User=mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
